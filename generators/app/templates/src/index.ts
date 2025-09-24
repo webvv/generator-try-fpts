4e76a5663bcc3f214f51610230080d3c3296a0db
webvv <webvillagevoyage@gmail.com>
2025-09-23 23:40:35 -0700

    improve template

--- generators/app/templates/src/index.ts
+++ generators/app/templates/src/index.ts
@@ -1,27 +1,15 @@
-import { pipe } from 'fp-ts/function'
 import * as R from 'fp-ts/Reader'
+import { pipe } from 'fp-ts/function'
 
-interface Config {
-  a: number,
-  b: string
-}
+interface Config { a: number, b: string }
+interface Config2 { url: string }
 
-interface Config2 {
-  url: string
-}
-
-const result1 = pipe(
-  R.ask<Config>(),
-  R.map(x => x.a),
-  R.map(a => a+1),
-)({ a: 10, b: ''})
-
-const result2 = pipe(
+const result = pipe(
   R.ask<Config>(),
   R.map(x => x.a),
   R.map(a => a+1),
   R.flatMap(x => R.ask<Config2>()),
   R.map(x => x.url)
-)({ a: 10, b: '', url: 'url1'})
+)({ a: 10, b: '', url: 'https://youtube.com/@webvv'})
 
-console.log(result2)
+console.log(result)
